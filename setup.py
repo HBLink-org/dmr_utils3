76a4601804ec2a66668b331b40193435ce25ff90
Cort Buffington <n0mjs@me.com>
2019-05-08 13:58:08 -0500

    Update Version Number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
         return file.read()
 
 setup(name='dmr_utils3',
-      version='0.1.25',
+      version='0.1.26',
       description='ETSI DMR (Digital Mobile Radio) Tier II Utilities',
       long_description='Modules to disassemble and assemble DMR packets, including generating and decoding various FEC routines',
       classifiers=[
